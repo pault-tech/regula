530260e7afbb6cf0dc7474c5df3dd4329d72e9c7
Paul T <pault-tech@github.com>
2023-06-05 02:06:30 +0000

    cleanup

--- aa.sh
+++
@@ -1,4 +0,0 @@
-echo adding missing devcontianer.json to this repo...
-
-devctemplate="ghcr.io/devcontainers/templates/go:latest"
-devcontainer --workspace-folder regula templates apply -t $devctemplate
--- devcontainer.sh
+++ devcontainer.sh
@@ -1,4 +1,14 @@
-echo adding missing devcontianer.json to this repo
+# echo adding missing devcontianer.json to this repo
 
-devctemplate="ghcr.io/devcontainers/templates/go:latest"
-devcontainer --workspace-folder regula templates apply -t $devctemplate
+# devctemplate="ghcr.io/devcontainers/templates/go:latest"
+# devcontainer --workspace-folder regula templates apply -t $devctemplate
+
+wget https://github.com/fugue/regula/releases/download/v3.2.1/regula_3.2.1_Linux_x86_64.tar.gz
+
+gunzip regula_3.2.1_Linux_x86_64.tar.gz
+
+tar -xvf regula_3.2.1_Linux_x86_64.tar
+
+./regula --version
+
+git clone https://github.com/awslabs/genomics-tertiary-analysis-and-machine-learning-using-amazon-sagemaker
